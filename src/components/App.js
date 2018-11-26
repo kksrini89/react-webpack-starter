b81af823a54ca1ef2814163de1bd7ea7a463f41b
kksrinivasan <srinivasan.kk@terralogic.com>
2018-11-26 19:39:21 +0530

    Initial update

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../components/App.css';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>My React App with Webpack Starter</h1>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new cleanWebpackPlugin(['dist']),
+    new htmlWebpackPlugin({ template: './src/index.html' })
+  ]
+};
